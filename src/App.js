05a1d8b30b867fa964020ff623956d02710cd528
srihar5ha <sri3har5ha@gmail.com>
2023-06-17 10:40:50 +0530

    added latest transaction details.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { useEffect, useState } from 'react';
 
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 // Refer to the README doc for more information about using API
 // keys in client-side code. You should never do this in production
 // level code.
@@ -21,16 +23,71 @@ const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [transactions,setTransactions]=useState();
+  // useEffect(() => {
+  //   async function getBlockNumber() {
+  //     setBlockNumber(await alchemy.core.getBlockNumber());
+  //   }
 
-  useEffect(() => {
-    async function getBlockNumber() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
+  //   getBlockNumber();
+  // });
+
+  async function getBlockNumber(){
+    let number=await alchemy.core.getBlockNumber();
+    setBlockNumber(number);
+
+     
+if (number){
+  async function getBlockWithTransactions(number){
+    const response = await alchemy.core.getBlockWithTransactions(number);
+    console.log(response.transactions.length);
+    let transactionObject={
+      'total': response.transactions.length ,
+      'from': response.transactions[0]?.['from'],
+      'to': response.transactions[0]?.['to']
     }
+    setTransactions(transactionObject);
+  }
 
-    getBlockNumber();
-  });
+  getBlockWithTransactions(blockNumber);
+}
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+
+  }
+ 
+useEffect(()=>{
+  getBlockNumber();
+
+},[]);
+
+  return <div className="App">
+     <div className="container mt-5 w-50">
+      <div className="card border rounded">
+        <div className="card-body">
+          <h6 className="card-title bg-warning p-2">Latest Block Number: {blockNumber}</h6>
+          <div className="bg-primary text-white p-2">
+            <h6 className="card-title">Transactions in the Block:</h6>
+            {transactions && (
+              Object.entries(transactions).map(([key, value]) => (
+                <div key={key}>
+                  <div className="card mb-2">
+                    <div className="card-body">
+                      <h6 className="card-title">{key}</h6>
+                      <p className="card-text">{value}</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+
+   
+
+     </div>;
+
 }
 
 export default App;
